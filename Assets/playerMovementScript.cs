f7caf3ab8af7ea81d24983706053bcd12f0c70a3
Tarin Fung <tfung101@gmail.com>
2023-05-15 09:49:48 -0400

    MORE PLAYER MOVEMENT CHANGES

--- Assets/playerMovementScript.cs
+++ Assets/playerMovementScript.cs
@@ -6,10 +6,12 @@ public class playerMovementScript : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] int jumpForce;
+    Transform transform;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -21,11 +23,13 @@ public class playerMovementScript : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            rb.velocity = new Vector2(-5f, rb.velocity.y);
+            //rb.velocity = new Vector2(-5f, rb.velocity.y);
+            transform.position = transform.position + new Vector3(-speed, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            rb.velocity = new Vector2(5f, rb.velocity.y);
+            //rb.velocity = new Vector2(5f, rb.velocity.y);
+            transform.position = transform.position + new Vector3(speed, 0, 0);
         }
     }
 }
