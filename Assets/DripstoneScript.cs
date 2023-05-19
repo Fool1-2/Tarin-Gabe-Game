21259a2ff5738beb6e4f41287d2902c55ef180cb
Tarin Fung <tfung101@gmail.com>
2023-05-19 09:55:00 -0400

    Dripstone Collider

---
+++ Assets/DripstoneScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DripstoneScript : MonoBehaviour
+{
+    public int difLevel;
+    Rigidbody2D rb;
+    gameManagerScript gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.gravityScale = 0.5f * difLevel;
+        gm = GameObject.Find("GMObject").GetComponent<gameManagerScript>();
+        SetGravSpeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gm.gameOver = true;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Border")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void SetGravSpeed()
+    {
+        int i = Random.Range(0, Mathf.RoundToInt(gm.gameTimer / 5));
+        if (i < 6)
+        {
+            rb.gravityScale = 0.5f;
+        }
+        else if (i < 10)
+        {
+            rb.gravityScale = 1;
+        }
+        else
+        {
+            rb.gravityScale = 2;
+        }
+    }
+
+}
--- Assets/gameManagerScript.cs
+++ Assets/gameManagerScript.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class gameManagerScript : MonoBehaviour
 {
     public float spawnTimer;
-    public GameObject bullet1;
-    public List<Vector3> spawnPositions;
-    public int spawnSpeed;
+    public GameObject dripstone;
+    public float spawnSpeed = 5;
+    public bool gameOver;
+    Rigidbody2D rb;
+
+    public float gameTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(checkSpawnSpeed());
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameTimer += Time.deltaTime;
         spawnTimer += Time.deltaTime;
         if (spawnTimer > spawnSpeed)
         {
+            //Creates a new dripstone at a random x position with a height of seven 
+            Instantiate(dripstone, new Vector3(Random.Range(-4.8f, 4.8f), 7, 0), Quaternion.identity);
             spawnTimer = 0;
-            Instantiate(bullet1, spawnPositions[Random.Range(0, 3)], Quaternion.identity);
         }
+        
     }
+    IEnumerator checkSpawnSpeed()
+    {
+        if (spawnSpeed > 1)
+        {
+            spawnSpeed = 5 - 0.05f;
+        }
+        Debug.Log("changed");
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(checkSpawnSpeed());
+    }
+
+
+
 }
